205b5c44181bc4d4b2860584d8d91c1a8b400d6d
Esther Manrique González <esmango0610@gmail.com>
2020-06-28 14:34:55 -0500

    Updates

---
+++ precache-manifest.a36c55d1b3e752c40f7ec94263caf384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7570722b71b37bcdfad12daa0aa7568",
+    "url": "/data-lovers-rick-and-morty/index.html"
+  },
+  {
+    "revision": "84ccf7402ab2b0d8bfdb",
+    "url": "/data-lovers-rick-and-morty/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e729f379e92d717fda44",
+    "url": "/data-lovers-rick-and-morty/static/css/main.fef4734f.chunk.css"
+  },
+  {
+    "revision": "84ccf7402ab2b0d8bfdb",
+    "url": "/data-lovers-rick-and-morty/static/js/2.232dc861.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/data-lovers-rick-and-morty/static/js/2.232dc861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e729f379e92d717fda44",
+    "url": "/data-lovers-rick-and-morty/static/js/main.54234a27.chunk.js"
+  },
+  {
+    "revision": "393d93aadc5d3fe252a1",
+    "url": "/data-lovers-rick-and-morty/static/js/runtime-main.edf9ba03.js"
+  }
+]);
